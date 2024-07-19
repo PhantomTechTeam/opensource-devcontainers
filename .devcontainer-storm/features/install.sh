d3cfd4320c8f9223d93f2a1781665364e98addc5
zenith110 <thequake@live.com>
2024-07-19 12:27:16 -0400

    Added github workflow

---
+++ .devcontainer-storm/features/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"vscode"}"}"
+USERHOME="/home/$USERNAME"
+if [ "$USERNAME" = "root" ]; then
+    USERHOME="/root"
+fi
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+if [ -f "${USERHOME}/.zshrc" ] then
+    printf "\n# k3d feature - dns-fix\nexport K3D_FIX_DNS=1\n" >> "${USERHOME}/.zshrc"
+fi
